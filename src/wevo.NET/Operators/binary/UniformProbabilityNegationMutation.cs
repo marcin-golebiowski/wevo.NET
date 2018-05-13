46d03fa7be8f06e78ed8d7f131ee95891e9aad78
Marcin <xormus@gmail.com>
2018-05-13 12:54:26 +0200

    Removing licensing from source files

--- src/wevo.NET/Operators/binary/UniformProbabilityNegationMutation.cs
+++ src/wevo.NET/Operators/binary/UniformProbabilityNegationMutation.cs
@@ -1,34 +1,13 @@
-﻿/*
- * wevo.NET - Distributed Evolutionary Computation Library
- *
- * Based on wevo and wevo2 libraries.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
-
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA  02110-1301  USA
- */
-
-/**
- * Performs mutation of the individual by randomly negating one 
- * bit with some probability.
- */
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using wevo.NET.Core.Individuals;
 using wevo.NET.Core.Utils;
 
 namespace wevo.NET.Core.Operators.Binary
 {
+    /**
+    * Performs mutation of the individual by randomly negating one 
+    * bit with some probability.
+    */
     public class UniformProbabilityNegationMutation : Operator<BinaryVector>
     {
         /** For each bit, probability of negating it. */
